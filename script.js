cf85a7180b4e5260d4d623949c929cd2c9138587
Mahesh Gaikwad <gaikwadmahesh777g@gmail.com>
2021-12-15 16:20:54 +0530

    added the code
    Just little styling is left

---
+++ script.js
@@ -0,0 +1,47 @@
+const popup = document.querySelector('.chat-popup');
+const chatBtn = document.querySelector('.chat-btn');
+const submitBtn = document.querySelector('.submit');
+const chatArea = document.querySelector('.chat-area');
+const inputElm = document.querySelector('input');
+
+const emojiBtn = document.querySelector('#emoji-btn');
+
+const picker = new EmojiButton();
+
+
+window.addEventListener('DOMContentLoaded',()=>{
+    
+    picker.on('emoji', emoji => {
+       document.querySelector('input').value += emoji;
+      });
+    
+    emojiBtn.addEventListener('click',()=>{
+        picker.togglePicker(emojiBtn);
+    });
+      
+});
+
+
+// Chat Button Toggler Starts
+
+chatBtn.addEventListener('click',()=>{
+    popup.classList.toggle('show');
+});
+
+// Chat Button Toggler Ends 
+
+//Send Message button start
+
+submitBtn.addEventListener('click',()=>{
+    let userInput = inputElm.value;
+    // console.log(userInput);
+    let temp = `<div class ="out-msg">
+        <span class="my-msg">${userInput}</span>
+        <i class="material-icons">account_circle</i>
+        </div>`;
+
+    chatArea.insertAdjacentHTML("beforeend",temp);
+    inputElm.value = '';
+})
+//Send Message button ends
+
